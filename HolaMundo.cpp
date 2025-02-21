a9cfd55cba5a3097bced4d2314d90e078b8d1f41
Willy Tenorio <wtenorio@gmail.com>
2025-02-21 10:56:04 -0400

    Primeros ejercicios

---
+++ HolaMundo.cpp
@@ -0,0 +1,21 @@
+/*
+    Creador: Willy Tenorio
+    Fecha creación: 07-08-2024
+    Descripción: Mostrar el funcionamiento de un programa en C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    system("cls"); // Limpiar Pantalla
+    cout << "MI PRIMER PROGRAMA (Version 2)\n";
+    cout << "==============================" << endl;
+    cout << "\tIngrese un numero: ";
+    cin >> numero;
+    cout << "\tEl numero ingresado es: " << numero;
+    return 0;
+}
