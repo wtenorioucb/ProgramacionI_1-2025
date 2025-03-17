859f9f7f263bca8acb128aabdcf4aa695639d420
Willy Tenorio P. <74077487+wtenorioucb@users.noreply.github.com>
2025-03-17 18:15:22 -0400

    Clase del 17-03-2025

--- Ejercicio_02_01.cpp
+++ Ejercicio_02_01.cpp
@@ -1,29 +1,59 @@
-// Materia: Programación I, Paralelo 3
-// Autor: Juan Jose Condori Yucra
-// Fecha creación: 25/02/2025
-// Número de ejercicio: 1
-// Problema planteado: Obtener la suma de la serie: 4 + 6 + 9 + 13 + 19 + 28 + 42 +……. Para n términos
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
+
 using namespace std;
-int main() {
-    int n;
-    cout << "Ingrese la cantidad de terminos: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "El numero de términos debe ser mayor a 0." << endl;
-        return 1;
-    }
-    int termino = 4, suma = 4;
-    int a = 2, b = 3;
-    cout << "Serie: " << termino << " ";
-    for (int i = 1; i < n; i++) {
-        termino += a;
-        suma += termino;
-        cout << termino << " ";
-        int nuevaDiferencia = a + b;
-        a = b;
-        b = nuevaDiferencia;
-    }
-    cout << "\nSuma de la serie: " << suma << endl;
+
+int tercerValor = 10;
+
+int SumarValores(int primerValor, int segundoValor);
+void MostrarEnPantalla(int posicion);
+int NroRandomico(int limiteInferior, int limiteSuperior);
+
+int main()
+{
+    int primerValor = 0;
+    int segundoValor = 0;
+    srand(time(NULL)); // Semilla
+    system("cls");
+    //MostrarEnPantalla(1);
+    //cin >> primerValor;
+    //MostrarEnPantalla(2);
+    //cin >> segundoValor;
+    primerValor = NroRandomico(1,100);
+    cout << "Valor 1: " << primerValor;
+    segundoValor = NroRandomico(1,100);
+    cout << "Valor 2: " << segundoValor << endl;
+    cout << "El resultado es: " << SumarValores(primerValor, segundoValor);
+    cout << "\nEl tercer valor: " << tercerValor;
     return 0;
+}
+
+int SumarValores(int primerValor, int segundoValor)
+{
+    int resultado = 0;
+    resultado = primerValor + segundoValor;
+    primerValor = resultado;
+    return resultado;
+}
+
+void MostrarEnPantalla(int posicion)
+{
+    cout << "Ingresar ";
+    switch (posicion)
+    {
+        case 1:
+            cout << "primer ";
+            break;
+        case 2:
+            cout << "segundo ";
+            break;
+    }
+    cout << "valor:";
+}
+
+int NroRandomico(int limiteInferior, int limiteSuperior)
+{
+    int aleatorio = limiteInferior + rand() % ((limiteSuperior + 1) - limiteInferior);
+    return aleatorio;
 }
